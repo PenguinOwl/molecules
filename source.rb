687a4df78a45f8850bb848a218e53a6ad4e9ebb9
PenguinOwl <375323@bsd48.org>
2016-09-20 19:22:36 -0700

    Add Molecule class

--- source.rb
+++ source.rb
@@ -1,2 +1,33 @@
 # This is the source file. The program runs here.
 
+length = 5
+height = 5
+
+def portgen
+  portid = []
+  length.times do
+    emt = rand(height-1) + 1
+    portid << emt.to_s
+  end
+  return portid.join
+end
+
+class Molecule
+  def initialize
+    @portcount = 1
+    ran = 0
+    while ran == 0
+      ran = rand(4)
+      @portcount += 1
+    end
+    @ports = []
+    @portcount.times do
+      @ports << portgen
+    end
+  end
+  def ports
+    return @ports
+  end
+  attr_accessor :name
+end
+
