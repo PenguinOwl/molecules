024e3a06ea84c8525ce5b2f5e40152c0adf247ac
PenguinOwl <henryjboles@gmail.com>
2016-11-03 18:31:53 -0700

    Fix var

--- update.rb
+++ update.rb
@@ -20,7 +20,7 @@ unless ARGV[0] == '--noupdate' or ARGV[0] == '-nu'
     exit
   else
     curfile.truncate(0)
-    content.each_line do |line|
+    contents.each_line do |line|
       curfile.write(line)
     end
     puts "Updated. Starting engine."
